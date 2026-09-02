0f0dfac4acd97eb515dbfb955417893e97122f76
Victor Bayona <victor.bayona@plaid.co.jp>
2026-09-02 17:32:03 +0900

    Release 0.21.3

--- ios/nubrick_flutter/Package.swift
+++ ios/nubrick_flutter/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/plaidev/nubrick-ios.git",
-            exact: "0.19.12"
+            exact: "0.19.14"
         )
     ],
     targets: [
